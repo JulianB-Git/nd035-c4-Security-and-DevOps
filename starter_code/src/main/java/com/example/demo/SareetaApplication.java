9115aa24c00165baf3877c9022ca698b1b765035
DESKTOP-VTIGGP1\Julian <julian.benade@gmail.com>
2021-07-03 18:19:49 +0200

    extend SpringBootServletInitializer from main application

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class SareetaApplication {
+public class SareetaApplication extends SpringBootServletInitializer{
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder(){
 		return new BCryptPasswordEncoder();
